75ef87dc770ebd132ecf99bb61426e104599342f
heredis12 <heredis12@gmail.com>
2024-05-23 01:22:12 +0800

    Update index

--- index.js
+++ index.js
@@ -1,50 +1,24 @@
-const express = require("express");
-const path = require("path");
-const TelegramBot = require("node-telegram-bot-api");
-const TOKEN = "7054554428:AAE3dy3KJ0M82JwMkaLyT5SyuM3gqDkAfK8";
-const server = express();
-const bot = new TelegramBot(TOKEN,{
-    polling:true
-});
-const port = process.env.PORT || 5000;
-const gameName = "infinitecrusade";
-const queries = {};
-server.use(express.static(path.join(__dirname,'infinitecrusade')));
-bot.onText(/help/,(msg) => bot.sendMessage(msg.from.id, "Say /game if you want to play."));
-bot.onText(/start|game/,(msg)=> bot.sendGame(msg.from.id,gameName));
-bot.on("callback_query", function(query){
-    if(query.game_short_name != gameName){
-        bot.answerCallbackQuery(query.id, "Sorry, '"+query.game_short_name+"' is not available.");    
-    }else{
-        bot.gameQuery((ctx) => {
-            let queryId = ctx.callbackQuery.id
-            let gameurl = "https://heredis12.github.io/infinitecrusade/"+queryId;
-            ctx.answerGameQuery(gameUrl)
-        })
-    }
-});
-bot.on("inline_query", function(iq){
-    bot.answerInlineQuery(iq.id,[{
-        type: "game",
-        id: "0",
-        game_short_name: gameName
-    }])
-});
-server.get("/highscore/:score", function (req, res, next) {
-    if (!Object.hasOwnProperty.call(queries, req.query.id)) return next();
-    let query = queries[req.query.id];
-    let options;
-    if (query.message) {
-        options = {
-            chat_id: query.message.chat.id,
-            message_id: query.message.message_id
-        };
-    } else {
-        options = {
-            inline_message_id: query.inline_message_id
-        };
-    }
-    bot.setGameScore(query.from.id, parseInt(req.params.score), options,
-        function (err, result) {});
-});
-server.listen(port);
+const { Telegraf, Markup } = require('telegraf')
+
+const token = "7054554428:AAE3dy3KJ0M82JwMkaLyT5SyuM3gqDkAfK8";
+if (token === undefined) {
+  throw new Error('BOT_TOKEN must be provided!')
+}
+
+const gameShortName = 'infinitecrusade'
+const gameUrl = 'https://heredis12.github.io/infinitecrusade/'
+
+const markup = Markup.inlineKeyboard([
+  Markup.button.game('🎮 Play now!'),
+  Markup.button.url('Telegraf help', 'http://telegraf.js.org')
+])
+
+const bot = new Telegraf(token)
+bot.start((ctx) => ctx.replyWithGame(gameShortName))
+bot.command('foo', (ctx) => ctx.replyWithGame(gameShortName, markup))
+bot.gameQuery((ctx) => ctx.answerGameQuery(gameUrl))
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
